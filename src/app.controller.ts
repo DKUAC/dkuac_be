671f997832e4190f6131c547e37b62140d913b6b
HoyeongJeon <hoyeong0302@gmail.com>
2024-07-01 20:41:58 +0900

    feat: 비밀번호 수정 구현

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,14 +1,12 @@
-import { Controller, Get, UseGuards } from '@nestjs/common';
+import { Controller, Get, UseGuards, Request } from '@nestjs/common';
 import { AppService } from './app.service';
-import { ApiProperty } from '@nestjs/swagger';
-import { JwtAuthGuard } from './auth/guards/jwt-auth.guard';
 
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
   @Get()
-  getHello(): string {
+  getHello(@Request() req): string {
     return this.appService.getHello();
   }
 }
--- src/auth/guards/jwt-auth.guard.ts
+++ src/auth/guards/jwt-auth.guard.ts
@@ -14,7 +14,7 @@ export class JwtAuthGuard extends AuthGuard('jwt') {
     return super.canActivate(context);
   }
 
-  handleRequest(err: any, user: any, info: any) {
+  handleRequest(err: any, user: any) {
     if (err || !user) {
       throw (
         err ||

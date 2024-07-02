8b3f67689666655335acf0fc81025b92bf09851a
HoyeongJeon <hoyeong0302@gmail.com>
2024-07-02 16:01:04 +0900

    refactor: 회원가입, 로그인 dto swagger 설정 추가

--- src/auth/dto/auth.dto.ts
+++ src/auth/dto/auth.dto.ts
@@ -9,9 +9,62 @@ export class SignUpDto extends PickType(UserModel, [
   'student_number',
   'birth',
   'phone',
-]) {}
+]) {
+  @ApiProperty({
+    description: '유저 이름',
+    default: '홍길동',
+  })
+  name: string;
+
+  @ApiProperty({
+    description: '성별',
+    default: 'M',
+    enum: ['M', 'F'],
+  })
+  gender: 'M' | 'F';
+
+  @ApiProperty({
+    description: '전공',
+    default: '컴퓨터공학',
+  })
+  major: string;
+
+  @ApiProperty({
+    description: `학번
+    - 아이디로 쓰일 예정
+    - 중복 불가능
+    - 숫자만 입력 가능(8자리)
+    `,
+    default: 32184045,
+  })
+  student_number: number;
+
+  @ApiProperty({
+    description: '생년월일',
+    default: '1998-03-02',
+  })
+  birth: Date;
+
+  @ApiProperty({
+    description: `전화번호, 초기 비밀번호로 쓰일 예정(e.g. 01012345678 -> 12345678(기본 비밀번호))`,
+    default: '01012345678',
+  })
+  phone: string;
+}
 
 export class LogInDto extends PickType(UserModel, [
   'student_number',
   'password',
-]) {}
+]) {
+  @ApiProperty({
+    description: '학번',
+    default: 32184045,
+  })
+  student_number: number;
+
+  @ApiProperty({
+    description: '비밀번호',
+    default: '12345678',
+  })
+  password: string;
+}

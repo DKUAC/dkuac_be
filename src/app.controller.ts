bab35095544e8f22b6ab3b3e558fdf596029434a
HoyeongJeon <hoyeong0302@gmail.com>
2024-08-29 01:36:48 +0900

    feat: 헬스체크 추가

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,5 +1,6 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Res } from '@nestjs/common';
 import { AppService } from './app.service';
+import { Response } from 'express';
 
 @Controller()
 export class AppController {
@@ -8,5 +9,10 @@ export class AppController {
   @Get()
   getHello(): string {
     return this.appService.getHello();
+  }
+
+  @Get('/health')
+  getHealth(@Res() res: Response) {
+    return res.status(200).send('ok');
   }
 }

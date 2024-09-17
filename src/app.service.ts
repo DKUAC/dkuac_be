17ab89fffe9175d9cf76098688b41cec1ec2e5cf
HoyeongJeon <hoyeong0302@gmail.com>
2024-09-17 14:32:51 +0900

    다시배포

--- src/app.service.ts
+++ src/app.service.ts
@@ -3,6 +3,6 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   getHello(): string {
-    return 'DKUAC 홈페이지 백엔드 서버입니다~!';
+    return 'DKUAC 홈페이지!';
   }
 }
